08ea9f50282c20658e8bdd2c5c77eda9f08c3a32
Safi Dewshi <safi.dewshi@gmail.com>
2020-04-22 11:36:40 +0100

    fixed a typo

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='gym_tictacktoe',
+setup(name='gym_tictactoe',
       version='0.0.1',
       install_requires=['gym', 'numpy']  # And any other dependencies tic-tac-toe environment needs
       )
